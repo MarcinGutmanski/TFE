84b1d59421cf1c680f3c738f09d19ceb30325b3f
Marcin Gutmanski <marcin@levelapp.be>
2022-03-20 16:19:39 +0100

    Affichage de produits tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">Creamates</a>
       </header>
-      <main>List of products</main>
+      <main>
+        <h1>List of Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.name}>
+              <a href={`/product/${product.name}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.name}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}€</strong>
+                </p>
+                <button>Add to basket</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
