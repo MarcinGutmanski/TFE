e0a6a2b317eeef88a4a8a1d2ee62c0398569fdd3
Marcin Gutmanski <marcin@levelapp.be>
2022-03-20 15:31:08 +0100

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">Creamates</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
